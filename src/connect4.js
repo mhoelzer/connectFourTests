a6d8ce8cac68dd4fe0d1d42f3bc39ce1a5e719ad
Michelle Hoelzer <hoelzermm@gmail.com>
2021-07-19 15:59:15 -0400

    got mocha

--- src/connect4.js
+++ src/connect4.js
@@ -168,59 +168,59 @@ initializeGame()
    Here are some tests that will help you ensure that your winner functions are working properly. There's no need to modify these! But you can add the tests you should create below.
 */
 
-const testWinnerVertical = function () {
-    console.group("Testing winnerVertical:")
-    console.assert((winnerVertical([
-        [ null, null, null, null, null, null, null ],
-        [ null, null, null, null, null, null, null ],
-        [ null, null, null, null, null, null, null ],
-        [ null, null, null, null, null, null, null ],
-        [ null, null, null, null, null, null, null ],
-        [ null, null, null, null, null, null, null ]
-    ]) === false), "Empty board")
-    console.assert((winnerVertical([
-        [ null, null, null, null, null, null, null ],
-        [ null, null, null, null, null, null, null ],
-        [    1, null, null, null, null, null, null ],
-        [    1, null, null, null, null, null, null ],
-        [    1, null, null, null, null, null, null ],
-        [    1, null, null, null, null, null, null ]
-    ]) === true), "Player 1 win on Column 1")
-    console.assert((winnerVertical([
-        [ null, null, null, null, null, null, null ],
-        [ null, null, null, null, null, null, null ],
-        [    2, null, null, null, null, null, null ],
-        [    2, null, null, null, null, null, null ],
-        [    2, null, null, null, null, null, null ],
-        [    2, null, null, null, null, null, null ]
-    ]) === true), "Player 2 win on Column 1")
-    console.assert((winnerVertical([
-        [ null, null, null, null, null, null, 2 ],
-        [ null, null, null, null, null, null, 2 ],
-        [ null, null, null, null, null, null, 2 ],
-        [ null, null, null, null, null, null, 2 ],
-        [ null, null, null, null, null, null, 1 ],
-        [ null, null, null, null, null, null, 1 ]
-    ]) === true), "Player 2 win on top of Column 7")
-    console.assert((winnerVertical([
-        [ null, null, null, null, null, null, 2 ],
-        [ null, null,    1, null, null, null, 2 ],
-        [ null, null,    1, null, null, null, 2 ],
-        [ null, null,    1, null, null, null, 2 ],
-        [ null, null,    1, null, null, null, 1 ],
-        [ null, null, null, null, null, null, 1 ]
-    ]) === true), "Player 1 win on middle of Column 3")
-    console.assert((winnerVertical([
-        [ null, null, null, null, null, null, null ],
-        [ null, null,    2, null, null, null, null ],
-        [    1, null,    2, null,    2, null, null ],
-        [    1, null,    2, null,    1, null, null ],
-        [    2,    2,    1,    1,    2, null,    2 ],
-        [    2,    1,    2,    1,    1,    2,    1 ]
-    ]) === false), "Random board with no winner yet")
-    console.groupEnd("Testing winnerVertical:")
-}
-testWinnerVertical()
+// const testWinnerVertical = function () {
+//     console.group("Testing winnerVertical:")
+//     console.assert((winnerVertical([
+//         [ null, null, null, null, null, null, null ],
+//         [ null, null, null, null, null, null, null ],
+//         [ null, null, null, null, null, null, null ],
+//         [ null, null, null, null, null, null, null ],
+//         [ null, null, null, null, null, null, null ],
+//         [ null, null, null, null, null, null, null ]
+//     ]) === false), "Empty board")
+//     console.assert((winnerVertical([
+//         [ null, null, null, null, null, null, null ],
+//         [ null, null, null, null, null, null, null ],
+//         [    1, null, null, null, null, null, null ],
+//         [    1, null, null, null, null, null, null ],
+//         [    1, null, null, null, null, null, null ],
+//         [    1, null, null, null, null, null, null ]
+//     ]) === true), "Player 1 win on Column 1")
+//     console.assert((winnerVertical([
+//         [ null, null, null, null, null, null, null ],
+//         [ null, null, null, null, null, null, null ],
+//         [    2, null, null, null, null, null, null ],
+//         [    2, null, null, null, null, null, null ],
+//         [    2, null, null, null, null, null, null ],
+//         [    2, null, null, null, null, null, null ]
+//     ]) === true), "Player 2 win on Column 1")
+//     console.assert((winnerVertical([
+//         [ null, null, null, null, null, null, 2 ],
+//         [ null, null, null, null, null, null, 2 ],
+//         [ null, null, null, null, null, null, 2 ],
+//         [ null, null, null, null, null, null, 2 ],
+//         [ null, null, null, null, null, null, 1 ],
+//         [ null, null, null, null, null, null, 1 ]
+//     ]) === true), "Player 2 win on top of Column 7")
+//     console.assert((winnerVertical([
+//         [ null, null, null, null, null, null, 2 ],
+//         [ null, null,    1, null, null, null, 2 ],
+//         [ null, null,    1, null, null, null, 2 ],
+//         [ null, null,    1, null, null, null, 2 ],
+//         [ null, null,    1, null, null, null, 1 ],
+//         [ null, null, null, null, null, null, 1 ]
+//     ]) === true), "Player 1 win on middle of Column 3")
+//     console.assert((winnerVertical([
+//         [ null, null, null, null, null, null, null ],
+//         [ null, null,    2, null, null, null, null ],
+//         [    1, null,    2, null,    2, null, null ],
+//         [    1, null,    2, null,    1, null, null ],
+//         [    2,    2,    1,    1,    2, null,    2 ],
+//         [    2,    1,    2,    1,    1,    2,    1 ]
+//     ]) === false), "Random board with no winner yet")
+//     console.groupEnd("Testing winnerVertical:")
+// }
+// testWinnerVertical()
 
 const testWinnerDiagonalUp = function () {
     console.group("Testing winnerDiagonalUp:")
